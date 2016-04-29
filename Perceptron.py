30ae82a8ae74691ed16206bf9a7dfcd3cec68c48
Christopher Jenness <Christopherjenness@gmail.com>
2016-04-29 08:28:25 -0400

    Added project outline

--- Perceptron.py
+++ Perceptron.py
@@ -1,1 +1,12 @@
+# Note: The perceptron requires linearly seperable data.
 # This will be my perceptron algorithm
+
+# h(x) = sign(transpose(w)*x)
+# input weights and vector
+
+### Algorithm
+# Randomly assign w
+# pick misclassified point (how to efficiently find?)
+# update weight vecotr w = w + yn*Xn
+# repeat until all are classified
+# return weights?
