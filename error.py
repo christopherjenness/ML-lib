467fac8a606065364b078ff73202974147673a12
Christopher Jenness <christopherjenness@gmail.com>
2016-05-21 19:27:58 -0400

    added mean classification error

--- error.py
+++ error.py
@@ -4,3 +4,8 @@ def MSE(y, predictions):
     n_samples = len(y)
     differences = y - predictions
     return np.dot(differences, differences.T) / n_samples
+
+def mean_classification_error(y, predictions):
+    n_samples = len(y)
+    correct_classifications = np.equal(y, predictions)
+    return (n_samples - correct_classifications.sum()) / n_samples
