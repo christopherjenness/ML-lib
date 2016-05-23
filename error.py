5efcbb46b24b905db12874eccd9dc5b207bf0c47
Christopher Jenness <christopherjenness@gmail.com>
2016-05-23 09:45:55 -0400

    added documentation

--- error.py
+++ error.py
@@ -1,11 +1,15 @@
+"""Common error measures for model predictions when true answer is known"""
+
 import numpy as np
 
-def MSE(y, predictions):
+def mse(y, predictions):
+    """Mean Square Error"""
     n_samples = len(y)
     differences = y - predictions
     return np.dot(differences, differences.T) / n_samples
 
 def mean_classification_error(y, predictions):
+    """Average classiication error"""
     n_samples = len(y)
     correct_classifications = np.equal(y, predictions)
     return (n_samples - correct_classifications.sum()) / n_samples
--- modelselection.py
+++ modelselection.py
@@ -1,13 +1,35 @@
+"""Useful algorithms for selecting models"""
+
 import numpy as np
-import error
-import linearregression
 
 def best_subset(X, y, model, parameters, error_measure, direction='forward'):
+    """
+    Function for selecting a subset of parametrs from X, which minimize the in sample
+    error measure.  Algorithm acts in a greedy manner, either adding one parameter at
+    a time (forward), or removing one at a time (backward).
+
+    Args:
+        X (np.ndarray): Training data of shape[n_samples, n_features]
+        y (np.ndarray): Target values of shape[n_samples, 1]
+        model (class): Machine Learning model containing fit and predict methods
+            For example LinearRegression from linearregression module
+        error_measure (function): function(y, predictions) for measuring the model fit.
+            For example, MSE error from error module
+        parameters (int): number of parameters in subset to return
+        direction: {'forward', 'backward'}
+            forward adds one parameter at a time in a greedy manner
+            backwards removes one parameter at a time in a greedy manner
+
+    Returns:
+        current_params (np.ndarray): shape[parameters, 1]
+        Returns best subset of parameters that minimize in sample error
+    """
+
     n_features = np.shape(X)[1]
     n_samples = np.shape(X)[0]
     if direction not in ['forward', 'backward']:
         raise  NameError("direction must be 'forward' or 'backward'")
-    if direction=='forward':
+    if direction == 'forward':
         param_count = 0
         current_params = []
         # Add one parameter at a time
@@ -17,10 +39,11 @@ def best_subset(X, y, model, parameters, error_measure, direction='forward'):
             best_error = np.inf
             # Loop through remaining feature to determine best
             for feature in range(n_features):
+                # only look at features not already selected
                 if feature not in current_params:
-                    test_column = X[:,feature]
+                    test_column = X[:, feature]
                     if len(current_params) > 0:
-                        test_array = np.column_stack((X[:,current_params], test_column))
+                        test_array = np.column_stack((X[:, current_params], test_column))
                     else:
                         test_array = test_column
                     classifier = model()
@@ -31,7 +54,7 @@ def best_subset(X, y, model, parameters, error_measure, direction='forward'):
             current_params.append(best_param)
             param_count += 1
         return current_params
-    if direction =='backward':
+    if direction == 'backward':
         param_count = n_features
         current_params = list(range(n_features))
         # Remove one parameter at a time
@@ -42,7 +65,7 @@ def best_subset(X, y, model, parameters, error_measure, direction='forward'):
             # Loop through remaining feature to determine least predictive
             for feature in range(n_features):
                 if feature in current_params:
-                    test_array = np.delete(X, feature, 1) 
+                    test_array = np.delete(X, feature, 1)
                     classifier = model()
                     classifier.fit(test_array, y)
                     test_error = error_measure(y, classifier.predict(test_array))
