e266677773b9e106631949ab0606925038f77a4a
Christopher Jenness <christopherjenness@gmail.com>
2016-05-26 08:37:34 -0600

    added predict algorithm

--- regularizeddiscriminantanalysis.py
+++ regularizeddiscriminantanalysis.py
@@ -4,10 +4,11 @@ linear discrimenent analysis and quadratic discrimenent analysis.
 """
 
 import numpy as np
+import itertools
 
 class RegularizedDisriminentAnalysis():
 
-    def __init__(self, alpha):
+    def __init__(self, alpha = 1):
         """
         Attributes::
             learned (bool): Keeps track of if Linear Regression has been fit
@@ -18,26 +19,43 @@ class RegularizedDisriminentAnalysis():
                 with seperate covariances of QDA.
         """
         self.learned = False
-        self.weights = np.NaN
         self.alpha = alpha
+        self.class_names = []
+        self.regularized_covariances = {}
+        self.class_priors = {}
+        self.class_means = {}
+        self.class_names = []
         
     def fit(self, X, y):
         n_samples = np.shape(X)[0]
         n_samples = np.shape(X)[1]
         n_classes = len(np.unique(y))
         
+        self.class_names = np.unique(y)
         class_covariances = {}
-        class_priors = {}
-        class_means = {}
         pooled_covariances = 0
         
-        for i in range(n_classes):
-            class_indices = np.where(y == i)
-            class_samples = X[class_samples, :]
+        for i in self.class_names:
+            class_indices = np.where(y == i)[0]
+            class_samples = X[class_indices, :]
             
-            class_priors[i] = float(len(class_indices)) / len(y)
-            class_means[i] = np.mean(class_samples, axis = 0)
+            self.class_priors[i] = float(len(class_indices)) / len(y)
+            self.class_means[i] = np.mean(class_samples, axis = 0)
             
             class_covariances[i] = np.cov(class_samples, rowvar=0)
+            #add contribution of individual class covariance to the pooled covariance)
+            pooled_covariances += class_covariances[i] * self.class_priors[i]
+        for i in self.class_names:
+            self.regularized_covariances[i] = self.alpha * class_covariances[i] + (1 - self.alpha) * pooled_covariances
+        self.learned = True
         
-        print (class_covariances)
+    def predict(self, x):
+        class_deltas = {}
+        for i in self.class_names:
+            # Divid the class delta calculation into 3 parts
+            part1 = -0.5 * np.linalg.det(self.regularized_covariances[i])
+            part2 = -0.5 * np.dot(np.dot((x - self.class_means[i]).T,  np.linalg.pinv(self.regularized_covariances[i])), (x - self.class_means[i]))
+            part3 = np.log(self.class_priors[i])
+            class_deltas[i] = part1 + part2 + part3
+        return max(class_deltas, key = class_deltas.get)
+        
