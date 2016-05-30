199bd326f2a08d429a5b3a0fefe4ab58cd3a9697
Christopher Jenness <christopherjenness@gmail.com>
2016-05-30 13:35:32 -0400

    added documentation

--- knearestneighbor.py
+++ knearestneighbor.py
@@ -1,32 +1,53 @@
 """
-
+K nearest neighbors
 """
 import numpy as np
 from scipy import stats
 
 class KNearestNeighbor(object):
     """
-    
+    K nearest neighbors classification and regression
     """
     def __init__(self):
         """
-        
+        Attributes:
+            samples (np.ndarray): Data of known target values
+            values (np.ndarray): Known target values for data
+            learned (bool): Keeps track of if model has been fit
         """
         self.samples = np.nan
         self.values = np.nan
         self.learned = False
-        
+
     def fit(self, X, y):
         """
-        
+        Args:
+            X (np.ndarray): Training data of shape[n_samples, n_features]
+            y (np.ndarray): Target values of shape[n_samples, 1]
+
+        Returns:
+            self: Returns an instance of self
         """
         self.samples = X
         self.values = y
         return self
-        
+
     def predict(self, x, k=1, model='regression'):
         """
-        Note: currenly only works on single vector and not matrix
+        Note: currenly only works on single vector and not matrices
+
+        Args:
+            x (np.ndarray): Training data of shape[1, n_features]
+            k (int): number of nearest neighbor to consider
+            model: {'regression', 'classification'}
+                K nearest neighbor classification or regression.
+                Choice most likely depends on the type of data the model was fit with.
+
+        Returns:
+            prediction (float): Returns predicted value
+
+        Raises:
+            ValueError if model has not been fit
         """
         distances = np.array([])
         for row in range(np.shape(self.samples)[0]):
