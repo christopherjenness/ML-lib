f419e09af247ca92314978ec897858754fc878dd
Christopher Jenness <christopherjenness@gmail.com>
2016-06-25 16:05:08 -0400

    added cross entropy error

--- error.py
+++ error.py
@@ -13,4 +13,11 @@ def mean_classification_error(y, predictions):
     n_samples = len(y)
     correct_classifications = np.equal(y, predictions)
     return (n_samples - correct_classifications.sum()) / n_samples
-
+    
+def cross_entropy_error(y, predictions):
+    """Cross Entropy Error"""
+    n_samples = len(y)
+    predictions[predictions==0] = 0.00000001
+    predictions[predictions==1] = 0.99999999
+    cross_entropy_errors = -y * np.log(predictions) - (1 - y) * np.log(1 - predictions)
+    return np.sum(cross_entropy_errors)
