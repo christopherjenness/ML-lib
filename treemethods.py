f5e68c9eaa30946b40f7770eccaac7519ab51db3
Christopher Jenness <christopherjenness@gmail.com>
2016-07-01 07:48:50 -0400

    initialized PRIM algorithm

--- treemethods.py
+++ treemethods.py
@@ -2,6 +2,7 @@ import numpy as np
 import networkx as nx
 import abc
 from scipy.stats import mode
+
 class BaseTree(object):
     """
     
@@ -209,6 +210,97 @@ class ClassificationTree(BaseTree):
                 node_indices = self.partition_data(i)
                 classval = mode(self.y[node_indices]).mode[0]
                 self.graph.node[i]['classval'] = classval   
+                
+class Prim(BaseTree):
+    """
+    Patient Rule Induction Method
+    """
+    def node_partition_data(self, node_number):
+        predecessors = self.get_predecessors(node_number)
+        predecessors.reverse()
+        predecessors.append(node_number)
+        data_indices = np.array(range(len(self.y)))
+        node_count = 0
+        while node_count < len(predecessors) - 1:
+            current_node = predecessors[node_count]
+            next_node = predecessors[node_count + 1]
+            cutoff_dict = self.graph.node[current_node]['cutoffs']
+            for key in cutoff_dict:
+                current_variable = key
+                current_cutoff_min = cutoff_dict[key][0]
+                current_cutoff_max = cutoff_dict[key][0]
+                boxed_data = data_indices[(self.X[data_indices, current_variable] < current_cutoff_max) & (self.X[data_indices, current_variable] > current_cutoff_min)]
+                if next_node == min(self.graph.successors(current_node)):
+                    data_indices = boxed_data
+                else:
+                    data_indices = np.delete(data_indices, boxed_data)
+                node_count +=1
+        return data_indices
+    
+    @staticmethod    
+    def partition_data(inputs, cutoff_dict):
+        data_indices = np.array(range(np.shape(inputs)[0]))
+        for key in cutoff_dict:
+            current_variable = key
+            current_cutoff_min = cutoff_dict[key][0]
+            current_cutoff_max = cutoff_dict[key][1]
+            boxed_data = data_indices[(inputs[data_indices, current_variable] < current_cutoff_max) & (inputs[data_indices, current_variable] > current_cutoff_min)]
+            data_indices = boxed_data
+            print(key, current_cutoff_min, boxed_data, data_indices)
+        return data_indices
+        
+        
+    def CART(self, inputs, values):
+        inputs = inputs
+        values = values
+        # Aim for box with 10% of initial box size
+        target_partition_size = int(len(values) * 0.1)
+        # Initalizes Boxes
+        # cutoffs is a dictionary where each key is a feature and each value is 
+        # a list [min_cutoff, max_cutoff]
+        cutoffs = {}
+        for feature in range(np.shape(inputs)[1]):
+            cutoffs[feature] = [-np.inf, np.inf]
+        response_mean = np.mean(values)
+        # Contracting phase
+        for i in range(4):
+            best_feature = None
+            best_cutoff = [-np.inf, np.inf]
+            response_mean_improvement = 0
+            for feature in range(np.shape(inputs)[1]):
+                feature_vector = inputs[:, feature]
+                sorted_vector = np.unique(np.sort(feature_vector))
+                feature_splits = (sorted_vector[1:] + sorted_vector[:-1]) / 2
+                lower_split = feature_splits[int(len(feature_splits) * 0.1)]
+                upper_split = feature_splits[int(len(feature_splits) * 0.9)]
+                upper_class_average = np.mean(values[feature_vector > lower_split])
+                lower_class_average = np.mean(values[feature_vector < upper_split])
+                max_average = max(upper_class_average, lower_class_average)
+                print(max_average, response_mean, response_mean_improvement)
+                if max_average - response_mean > response_mean_improvement:
+                    response_mean_improvement = max_average - response_mean
+                    best_feature = feature
+                    if upper_class_average > lower_class_average:
+                        best_cutoff = [lower_split, np.inf]
+                    else:
+                        best_cutoff = [-np.inf, upper_split]
+            cutoffs[best_feature] = best_cutoff
+            boxed_indices = self.partition_data(inputs, cutoffs)
+            inputs = inputs[boxed_indices, :]
+            values = values[boxed_indices]
+        return cutoffs
+        
+    def add_split(self, node_number, data, values):
+        cutoffs = self.CART(data, values)
+        self.set_node(node_number, min_feature, min_split)
+        self.new_nodes(node_number, 2)
+                        
+                        
+                        
+                        
+                        
+
+    
     
     
     
