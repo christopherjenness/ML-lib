03a66c3af63ab5cabbcc0ec92f8def09c84f2280
Christopher Jenness <christopherjenness@gmail.com>
2016-07-03 16:35:20 -0400

    initialized setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ML',
+    version='0.1',
+    packages=['ml',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
