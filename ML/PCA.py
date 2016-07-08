25459ce46fa2d20832260ccffb97710a411ead8a
Christopher Jenness <christopherjenness@gmail.com>
2016-07-08 16:53:11 -0400

    Add documentation

--- ML/PCA.py
+++ ML/PCA.py
@@ -1,18 +1,33 @@
 """
 Exact principal component analysis (PCA)
 """
+import numpy as np
 
 class PCA(object):
     """
-    Exact principal component analysis (PCA)
+    Exact principal component analysis (PCA).  Transforms given data set
+    into orthonormal basis, maximizing variance.
+    TODO return variance explained by each principal component
     """
 
     def __init__(self):
+        """
+        Attributes:
+            X (np.ndarray): Training data of shape[n_samples, n_features]
+            X_normalized (np.ndarray): Mean normalized data of shape[n_samples, n_features]
+            transformed_X (np.ndarray): Transformed data of shape[n_samples, n_features]
+                This data has contains all principal components, in order of variance explained
+        """
         self.X = None
         self.X_normalized = None
         self.transformed_X = None
 
     def fit(self, X):
+        """
+        Args:
+            X (np.ndarray): Training data of shape[n_samples, n_features]
+        Returns: an instance of self
+        """
         self.X = X
         self.mean_normalize()
         cov_matrix = (np.dot(self.X_normalized.T, self.X_normalized)) * 1/(np.shape(self.X)[0] - 1)
@@ -20,12 +35,15 @@ class PCA(object):
         eigen_vectors = eigen_vectors.T
         # Sort the eigen vectors
         eigen_order = np.argsort(eigen_vals)
-        P = eigen_vectors[:,eigen_order]
+        P = eigen_vectors[:, eigen_order]
         print(cov_matrix, P)
         # Project X onto PC
         self.transformed_X = (np.dot(P, X.T)).T
         return self
 
     def mean_normalize(self):
+        """
+        Normalizes input data (subtracts mean from each sample)
+        """
         col_means = self.X.sum(axis=0) / np.shape(self.X)[0]
         self.X_normalized = self.X - col_means
