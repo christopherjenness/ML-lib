44e3876d76c7d7b3571c82030ff78260e4ec7e65
Christopher Jenness <christopherjenness@gmail.com>
2016-07-08 06:08:30 -0400

    Add PCA.py template

---
+++ ML/PCA.py
@@ -0,0 +1,14 @@
+"""
+Exact principal component analysis (PCA)
+"""
+
+class PCA(object):
+    """
+    Exact principal component analysis (PCA)
+    """
+    
+    def __init__(self):
+        return 
+    
+    def fit(self, X):
+        return 
