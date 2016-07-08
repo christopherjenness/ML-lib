ea9a1d11a9665d86ecc0a4baa78be44d4fc37887
Christopher Jenness <christopherjenness@gmail.com>
2016-07-08 07:13:47 -0400

    Finish PCA

--- ML/PCA.py
+++ ML/PCA.py
@@ -6,9 +6,26 @@ class PCA(object):
     """
     Exact principal component analysis (PCA)
     """
-    
+
     def __init__(self):
-        return 
-    
+        self.X = None
+        self.X_normalized = None
+        self.transformed_X = None
+
     def fit(self, X):
-        return 
+        self.X = X
+        self.mean_normalize()
+        cov_matrix = (np.dot(self.X_normalized.T, self.X_normalized)) * 1/(np.shape(self.X)[0] - 1)
+        eigen_vals, eigen_vectors = np.linalg.eig(cov_matrix)
+        eigen_vectors = eigen_vectors.T
+        # Sort the eigen vectors
+        eigen_order = np.argsort(eigen_vals)
+        P = eigen_vectors[:,eigen_order]
+        print(cov_matrix, P)
+        # Project X onto PC
+        self.transformed_X = (np.dot(P, X.T)).T
+        return self
+
+    def mean_normalize(self):
+        col_means = self.X.sum(axis=0) / np.shape(self.X)[0]
+        self.X_normalized = self.X - col_means
