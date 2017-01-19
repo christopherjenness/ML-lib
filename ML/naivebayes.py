aa507125703365e665c79a1a0feae28f3be7f213
Christopher Jenness <christopherjenness@gmail.com>
2017-01-19 07:48:56 -0500

    Add Gaussian NB fit method

--- ML/naivebayes.py
+++ ML/naivebayes.py
@@ -6,12 +6,12 @@ Includes gaussian, bernoulli and multinomial models
 import abc
 import numpy as np
 
-class NaiveBayes:
+class NaiveBayes():
     """
     Naive Bayes Classifier
     Given class label, assumes features are independent
     """
-     __metaclass__ = abc.ABCMeta
+    __metaclass__ = abc.ABCMeta
 
     def __init__(self):
         """
@@ -55,4 +55,42 @@ class NaiveBayes:
             ValueError if model has not been fit
         """
         return self
+        
+class GaussianNaiveBayes(NaiveBayes):
+    
+    def __init__(self):
+        super().__init__()
+        
+    def fit(self, X, y):
+        n_samples = len(y)
+        class_names = list(np.unique(y))
+        for class_name in class_names:
+            # Compute class priors
+            class_prior = float(len(y[y == class_name]) / n_samples)
+            self.class_priors[class_name] = class_prior
+            
+            # Compute class mean and variance
+            # Assume features are independent given class label
+            mu = list(np.mean(X[y==0,:], axis=0))
+            variance = list(np.var(X[y==0,:], axis=0))
+            self.class_parameters[class_name] = [mu, variance]
+            
+        print(self.class_priors)
+        print (self.class_parameters)
+ 
+### Begin Scratch Work       
+y = np.array([0, 0, 1, 1, 0, 0, 2, 2])
+x1 = np.array([np.random.normal() for i in range(8)])
+x2 = np.array([np.random.normal() for i in range(8)])
+X = np.array([x1, x2]).T
+
+a = GaussianNaiveBayes()
+a.fit(X, y)    
+
+
+    
+    
+    
+
+
 
