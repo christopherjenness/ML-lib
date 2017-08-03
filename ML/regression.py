8431a59d5d6fe215c48f42002f708fc2211fb4fa
Christopher Jenness <christopherjenness@gmail.com>
2017-08-03 08:08:20 -0400

    Clean with flake8

--- ML/regression.py
+++ ML/regression.py
@@ -7,6 +7,7 @@ Includes closed form, gradient descent, and SGD solvers.
 import numpy as np
 from descentmethods import gradientdescent
 
+
 class LinearRegression:
     """Class for implimenting Linear Regression"""
     def __init__(self):
@@ -31,7 +32,7 @@ class LinearRegression:
         """
         if not self.learned:
             raise NameError('Fit model first')
-        # Add column of 1s to X for perceptron threshold
+        # Add column of 1s to X for bias
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
         prediction = np.dot(X, np.transpose(self.weights))
@@ -40,31 +41,34 @@ class LinearRegression:
     def grad(self, X, y, weights):
         """
         Computes the gradient (needed if using gradient descent).
-        
+
         Args:
             X (np.ndarray): Training data of shape[n_samples, n_features]
             y (np.ndarray): Target values of shape[n_samples, 1]
-            weights (np.ndarray): Optional use of gradient descent to calculate weights
+            weights (np.ndarray): Optional use of gradient descent to
+                calculate weights.
                 if False, uses closed form solution to calculate weights.
 
         Returns:
             np.array: the gradient of the linear regression cost function
         """
         hypothesis = np.dot(X, weights) - y
-        gradient = np.dot(np.transpose(X), hypothesis)  / np.size(y)
+        gradient = np.dot(np.transpose(X), hypothesis) / np.size(y)
         return gradient
 
     def fit(self, X, y, gradient=False, reg_parameter=0):
         """
         Currently, only L2 regularization is implemented.
-        
+
         Args:
             X (np.ndarray): Training data of shape[n_samples, n_features]
             y (np.ndarray): Target values of shape[n_samples, 1]
-            gradient (bool): Optional use of gradient descent to calculate weights
+            gradient (bool): Optional use of gradient descent to
+                calculate weights.
                 if False, uses closed form solution to calculate weights.
-            reg_parameter (float): float to determine strength of regulatrization  penalty
-                if 0, then no linear regression without regularization is performed
+            reg_parameter (float): float to determine strength of
+            regulatrization  penalty if 0, then no linear regression
+            without regularization is performed.
 
         Returns: an instance of self
         """
@@ -72,13 +76,17 @@ class LinearRegression:
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
         if gradient:
-            self.weights = gradientdescent(X, y, self.grad, reg_param=reg_parameter)
+            self.weights = gradientdescent(X, y, self.grad,
+                                           reg_param=reg_parameter)
         else:
-            #Calculate weights (closed form solution)
-            XtX_lambaI = np.dot(np.transpose(X), X) + reg_parameter * np.identity(len(np.dot(np.transpose(X), X)))
-            self.weights = np.dot(np.linalg.pinv(XtX_lambaI), np.dot(np.transpose(X), y))
+            # Calculate weights (closed form solution)
+            XtX_lambaI = np.dot(np.transpose(X), X) + reg_parameter * \
+                         np.identity(len(np.dot(np.transpose(X), X)))
+            self.weights = np.dot(np.linalg.pinv(XtX_lambaI),
+                                  np.dot(np.transpose(X), y))
         self.learned = True
         return self
+
 
 class LogisticRegression:
     """Logistic Regression classifier with gradient descent implementation"""
@@ -107,14 +115,15 @@ class LogisticRegression:
         Args:
             X (np.ndarray): Training data of shape[n_samples, n_features]
             y (np.ndarray): Target values of shape[n_samples, 1]
-            weights (np.ndarray): Optional use of gradient descent to calculate weights
+            weights (np.ndarray): Optional use of gradient descent to
+            calculate weights.
                 if False, uses closed form solution to calculate weights.
 
         Returns:
             np.ndarray: the gradient of the linear regression cost function
         """
         hypothesis = self.logistic_function(np.dot(X, weights)) - y
-        gradient = np.dot(np.transpose(X), hypothesis)  / np.size(y)
+        gradient = np.dot(np.transpose(X), hypothesis) / np.size(y)
         return gradient
 
     def predict(self, X):
@@ -130,21 +139,24 @@ class LogisticRegression:
         """
         if not self.learned:
             raise NameError('Fit model first')
-        # Add column of 1s to X for perceptron threshold
+        # Add column of 1s to X for bias
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
-        prediction = self.logistic_function(np.dot(X, np.transpose(self.weights)))
+        prediction = self.logistic_function(np.dot(X,
+                                                   np.transpose(self.weights)))
         return np.round(prediction)
 
     def fit(self, X, y, reg_parameter=0):
         """
         Currently, only L2 regularization is implemented.
-        
+
         Args:
             X (np.ndarray): Training data of shape[n_samples, n_features]
             y (np.ndarray): Target values of shape[n_samples, 1]
-            reg_parameter (float): float to determine strength of regulatrization  penalty
-                if 0, then no linear regression without regularization is performed
+            reg_parameter (float): float to determine strength of
+                regulatrization  penalty.
+                if 0, then no linear regression without regularization
+                is performed
 
         Returns: an instance of self
 
@@ -156,6 +168,7 @@ class LogisticRegression:
             raise NameError('y required to contain only 0 and 1')
         X = np.asarray(X)
         X = np.column_stack((np.ones(np.shape(X)[0]), X))
-        self.weights = gradientdescent(X, y, self.grad, reg_param=reg_parameter)
+        self.weights = gradientdescent(X, y, self.grad,
+                                       reg_param=reg_parameter)
         self.learned = True
         return self
