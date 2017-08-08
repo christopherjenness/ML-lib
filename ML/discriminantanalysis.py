899d49007f7fa8b8352eaca74912948f8f8dd62c
Christopher Jenness <christopherjenness@gmail.com>
2017-08-08 06:38:45 -0400

    Fix typo, flake8 clean

--- ML/discriminantanalysis.py
+++ ML/discriminantanalysis.py
@@ -9,7 +9,8 @@ diagonal (independent features), use Naive Bayes instead.
 
 import numpy as np
 
-class DisriminentAnalysis():
+
+class DiscriminentAnalysis():
     """
     Class for implimenting Regularized Discriminent Analysis
     LDA is performed when alpha == 1
@@ -19,19 +20,22 @@ class DisriminentAnalysis():
         """
         Args:
             alpha (float): degree of regularization, in range(0, 1)
-        
+
         Attributes:
             learned (bool): Keeps track of if RDA has been fit
             alpha (float): regularization parameter in range [0, 1]
-                When alpha == 1, LDA is performed, when alpha == 2, QDA is performed.
+                When alpha == 0, LDA is performed,
+                When alpha == 1, QDA is performed.
                 Intermediate values of alpha tradeoff pooled covarainces of LDA
-                with seperate covariances of QDA.
+                with seperate covariances of QDA, (regularized discriminant
+                analysis).
             class_names (np.ndarray): array of class names. [0, 1] for example.
             class_priors (dict): prior probability of each class.
                 determined via fraction of training samples in each class
             class_means (dict): vector means of each class
             regularized_covariances (np.ndarray): RDA covariances.
-                weighted combination of QDA class covariance and LDA pooled covariance
+                weighted combination of QDA class covariance and
+                LDA pooled covariance
         """
         self.learned = False
         self.alpha = alpha
@@ -51,18 +55,19 @@ class DisriminentAnalysis():
         self.class_names = np.unique(y)
         class_covariances = {}
         pooled_covariances = 0
-        #calculate class priors, class means, and LDA pooled covariance matrix
+        # Calculate class priors, class means, and LDA pooled covariance matrix
         for i in self.class_names:
             class_indices = np.where(y == i)[0]
             class_samples = X[class_indices, :]
             self.class_priors[i] = float(len(class_indices)) / len(y)
             self.class_means[i] = np.mean(class_samples, axis=0)
             class_covariances[i] = np.cov(class_samples, rowvar=0)
-            #add contribution of individual class covariance to the pooled covariance)
+            # Add contribution of individual class covariance
             pooled_covariances += class_covariances[i] * self.class_priors[i]
-        #calculate RDA regularized covariance matricies for each class
+        # Calculate RDA regularized covariance matricies for each class
         for i in self.class_names:
-            self.regularized_covariances[i] = self.alpha * class_covariances[i] + (1 - self.alpha) * pooled_covariances
+            self.regularized_covariances[i] = self.alpha * \
+                class_covariances[i] + (1 - self.alpha) * pooled_covariances
         self.learned = True
         return self
 
@@ -71,7 +76,7 @@ class DisriminentAnalysis():
         Args:
             x (np.array): Training data of shape[1, n_features]
                 Currently, only vector of single sample is supported
-                
+
         Returns: predicted class of sample
 
         Raises:
@@ -79,12 +84,15 @@ class DisriminentAnalysis():
         """
         if not self.learned:
             raise NameError('Fit model first')
-        #Determine probability of each class given input vector
+        # Determine probability of each class given input vector
         class_deltas = {}
         for i in self.class_names:
             # Divid the class delta calculation into 3 parts
             part1 = -0.5 * np.linalg.det(self.regularized_covariances[i])
-            part2 = -0.5 * np.dot(np.dot((x - self.class_means[i]).T, np.linalg.pinv(self.regularized_covariances[i])), (x - self.class_means[i]))
+            part2 = -0.5 * \
+                np.dot(np.dot((x - self.class_means[i]).T,
+                              np.linalg.pinv(self.regularized_covariances[i])),
+                       (x - self.class_means[i]))
             part3 = np.log(self.class_priors[i])
             class_deltas[i] = part1 + part2 + part3
         return max(class_deltas, key=class_deltas.get)
