25b0164b78298475513a45e7a6d5574d32c280f7
Christopher Jenness <christopherjenness@gmail.com>
2017-08-09 06:57:24 -0400

    Rename tests to avoid name re-use

--- tests/test_naivebayes.py
+++ tests/test_naivebayes.py
@@ -1,6 +1,5 @@
 import ML.naivebayes as naivebayes
 import data
-import numpy as np
 
 
 def test_gaussian_naive_bayes():
@@ -11,12 +10,14 @@ def test_gaussian_naive_bayes():
         predicted_y = nb.predict(row)
         assert predicted_y == y[index]
 
-def test_gaussian_naive_bayes():
+
+def test_gaussian_naive_bayes_probs():
     X, y = data.categorical_2Dmatrix_data()
     nb = naivebayes.GaussianNaiveBayes()
     nb.fit(X, y)
     y_probabilities = nb.predict(X[0], probabilities=True)
     assert y_probabilities[y[0]] == 1.0
+
 
 def test_bernoulli_naive_bayes():
     X, y = data.categorical_2Dmatrix_data()
@@ -26,7 +27,8 @@ def test_bernoulli_naive_bayes():
         predicted_y = nb.predict(row)
         assert predicted_y == y[index]
 
-def test_bernoulli_naive_bayes():
+
+def test_bernoulli_naive_bayes_probs():
     X, y = data.categorical_2Dmatrix_bernoulli_data()
     nb = naivebayes.BernoulliNaiveBayes()
     nb.fit(X, y)
