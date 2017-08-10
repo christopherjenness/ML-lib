644fa8647e592aa98f29f4dbacd23edbf43c4a7a
Christopher Jenness <christopherjenness@gmail.com>
2017-08-10 07:25:14 -0400

    Initialize SVM testing

---
+++ tests/test_svm.py
@@ -0,0 +1,23 @@
+import ML.svm as svm
+import data
+import numpy as np
+
+
+def test_SupportVectorMachine():
+    X, y = data.categorical_2Dmatrix_data_big()
+    SVM = svm.SupportVectorMachine()
+    SVM.fit(X, y)
+    prediction = SVM.predict(X[0])
+    assert prediction == y[0]
+    prediction = SVM.predict(X[-1])
+    assert prediction == y[-1]
+
+
+def test_SupportVectorMachine_polynomial_kernel():
+    X, y = data.categorical_2Dmatrix_data_big()
+    SVM = svm.SupportVectorMachine(kernel=svm.polynomial_kernel)
+    SVM.fit(X, y)
+    prediction = SVM.predict(X[0])
+    assert prediction == y[0]
+    prediction = SVM.predict(X[-1])
+    assert prediction == y[-1]
