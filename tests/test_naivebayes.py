fad1024d4751a5b6dfdf9e1a704ff801fa619b42
Christopher Jenness <christopherjenness@gmail.com>
2017-08-10 05:30:01 -0400

    Fix bernoulli testing

--- tests/test_naivebayes.py
+++ tests/test_naivebayes.py
@@ -23,9 +23,8 @@ def test_bernoulli_naive_bayes():
     X, y = data.categorical_2Dmatrix_data()
     nb = naivebayes.BernoulliNaiveBayes()
     nb.fit(X, y)
-    for index, row in enumerate(X):
-        predicted_y = nb.predict(row)
-        assert predicted_y == y[index]
+    predicted_y = nb.predict(X[0])
+    assert predicted_y == y[0]
 
 
 def test_bernoulli_naive_bayes_probs():
