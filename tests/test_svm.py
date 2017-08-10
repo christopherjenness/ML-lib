55f46e0e328838bde93b285fa9f7e77e9ce49240
Christopher Jenness <christopherjenness@gmail.com>
2017-08-10 07:31:42 -0400

    Fix bug (SVM targets are -1 and 1)

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def test_SupportVectorMachine():
     X, y = data.categorical_2Dmatrix_data_big()
+    y = (y * 2) - 1
     SVM = svm.SupportVectorMachine()
     SVM.fit(X, y)
     prediction = SVM.predict(X[0])
@@ -15,6 +16,7 @@ def test_SupportVectorMachine():
 
 def test_SupportVectorMachine_polynomial_kernel():
     X, y = data.categorical_2Dmatrix_data_big()
+    y = (y * 2) - 1
     SVM = svm.SupportVectorMachine(kernel=svm.polynomial_kernel)
     SVM.fit(X, y)
     prediction = SVM.predict(X[0])
