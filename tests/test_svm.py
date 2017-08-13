72b4d3e006e39ff1c5f2e38abc00dd1fb94c276d
Christopher Jenness <christopherjenness@gmail.com>
2017-08-13 13:52:46 -0400

    Add kernel tests

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -32,3 +32,28 @@ def test_Perceptron():
     perceptron.fit(X, y)
     predictions = perceptron.predict(X)
     assert (predictions == y).sum() > 7
+
+
+def test_linear_kernel():
+    x1 = [1, 2, 3]
+    x2 = [2, 2, 3]
+    kernel = svm.linear_kernel()
+    assert kernel(x1, x2) == 15
+
+
+def test_polynomial_kernel():
+    x1 = [1, 2, 3]
+    x2 = [2, 2, 3]
+    kernel = svm.polynomial_kernel(1, 1)
+    assert kernel(x1, x2) == 16
+    kernel = svm.polynomial_kernel(2, 2)
+    assert kernel(x1, x2) == 289
+
+
+def test_rbf_kernel():
+    x1 = np.array([1, 2, 3])
+    x2 = np.array([2, 2, 3])
+    kernel = svm.rbf_kernel(1)
+    np.testing.assert_almost_equal(kernel(x1, x2), 0.36787, 2)
+    kernel = svm.rbf_kernel(2)
+    np.testing.assert_almost_equal(kernel(x1, x2), 0.13533, 2)
